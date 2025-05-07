a3ad07bed507d5f6dad6cd3629e5e5312326ae24
guired13 <red2006135@gmail.com>
2025-05-08 00:05:28 +0800

    Add Hive initialization and ExpenseAdapter for expense data management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/expense.dart';
+
+
 
 void main() {
   runApp(ExpenseTrackerApp());
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ExpenseAdapter());
+  await Hive.openBox<Expense>('expensesBox');
+
+  runApp(MyApp());
 }
 
 class ExpenseTrackerApp extends StatelessWidget {
