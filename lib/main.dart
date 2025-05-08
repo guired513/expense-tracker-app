d49962fe175d56414c534c2f42687c22a4df7302
guired13 <red2006135@gmail.com>
2025-05-08 15:36:08 +0800

    Refactor main function to initialize Hive and register ExpenseAdapter before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,25 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'models/expense.dart';
 
 
-
-void main() {
-  runApp(ExpenseTrackerApp());
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-
   Hive.registerAdapter(ExpenseAdapter());
   await Hive.openBox<Expense>('expensesBox');
 
-  runApp(MyApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      home: HomeScreen(),
+    );
+  }
 }
 
 class ExpenseTrackerApp extends StatelessWidget {
